1f640cd7a1278a0f70bba570faf6ccad3d2d1d17
xiaoqingb <975947596@qq.com>
2020-01-20 23:29:54 +0800

    feat: 完成登录注册页面开发

---
+++ application/index/controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return view();
+    }
+    public function login()
+    {
+        return view();
+    }
+    public function regist()
+    {
+        return view();
+    }
+}
