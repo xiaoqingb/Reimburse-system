5c84613340abf3c1b8dd34e4156ec06e8b5784eb
xiaoqingb <975947596@qq.com>
2020-02-20 19:02:35 +0800

    feat：完成前台提交表单api开发

--- application/index/controller/Upload.php
+++ application/index/controller/Upload.php
@@ -7,30 +7,14 @@ use app\index\model\Upload as UploadModel;
 class Upload extends Controller
 {
     public function uploadImg(){
-        // $fn = isset($_SERVER['HTTP_X_FILENAME']) ? $_SERVER['HTTP_X_FILENAME'] : false ;
-        // if ($fn) {
-        //     file_put_contents(
-        //         ROOT_PATH . 'public' . DS . 'uploads/'.$fn,
-        //         file_get_contents('php://input')
-        //     );
-        //     // echo "http://www.zhangxinxu.com/study/201109/uploads/$fn";
-        //     return json_encode([
-        //         'code' => '0000',
-        //         'msg' => 'http://www.zhangxinxu.com/study/201109/uploads/$fn"'
-        //     ]);
-        // }
-        // return '0';
         $file = request()->file('file');
-        // foreach($files as $file){
-        // }
         if(empty($file)){
             $result["code"] = "0001";
             $result["msg"] = "文件不存在";
         }else{
             // 移动到框架应用根目录/public/uploads/ 目录下
-            $info = $file->move(ROOT_PATH . 'public' .DS. 'uploads/editor','');
+            $info = $file->move(ROOT_PATH . 'public/uploads/'.date(("Ymd")),'');
             if($info){
-                // $name_path =$info->getFilename();
                 //成功上传后 获取上传信息
                 $name_path =str_replace('\\',"/",$info->getSavename());
                 $result["code"] = '0000';
